8a2a5301812e2eea27bc3a7f3c8cd794c359277d
Casey Benzel <casey.benzel@gmail.com>
2012-10-16 00:20:09 -0400

    Push up openID Login files.

---
+++ se491project/src/edu/depaul/se491/servlets/login/LoginServlet.java
@@ -0,0 +1,29 @@
+package edu.depaul.se491.servlets.login;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class LoginServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher view = request.getRequestDispatcher("WEB-INF/webpages/login/openidlogin.jsp");
+		if (request.getParameter("continue") != null) {
+			request.setAttribute("continue", request.getParameter("continue"));			
+		} else {
+			request.setAttribute("continue", request.getRequestURI());
+		}	
+				
+		view.forward(request, response);
+		
+	}
+}
