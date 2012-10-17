b8c125f339266fdba6d798db51f580404efb5632
Adrian Petras <petrasadi@gmail.com>
2012-10-17 12:53:29 -0500

    - added OpenTok module and related libs

---
+++ se491project/war/javascript/opentok.js
@@ -0,0 +1,64 @@
+function endOpenTokSession() {
+	$.post('opentok', {param: 'forget'},
+		function () {
+			//TODO - fix me
+			$("#myPublisherDiv").html();
+		}
+	);
+}
+
+function getSessionInfo() {
+	$.post('opentok', {param: 'info'} ,
+		function (data) {
+			//alert(data.apiKey);
+			handleOpenTok(data);
+		},
+		'json'
+	);
+}
+
+function handleOpenTok(data){
+	var apiKey = data.apiKey;
+	var sessionId = data.sessionId;
+	var token = data.token;
+	
+	TB.setLogLevel(TB.DEBUG);
+	
+	var session = TB.initSession(sessionId);      
+	session.addEventListener('sessionConnected', sessionConnectedHandler);
+	session.addEventListener('streamCreated', streamCreatedHandler);      
+	session.connect(apiKey, token);
+	
+	var publisher;
+	
+	function sessionConnectedHandler(event) {
+		publisher = TB.initPublisher(apiKey, 'myPublisherDiv');
+		session.publish(publisher);
+		 
+		// Subscribe to streams that were in the session when we connected
+		subscribeToStreams(event.streams);
+	}
+	
+	function streamCreatedHandler(event) {
+		// Subscribe to any new streams that are created
+		subscribeToStreams(event.streams);
+	}
+	   
+	function subscribeToStreams(streams) {
+		for (var i = 0; i < streams.length; i++) {
+			// Make sure we don't subscribe to ourself
+			if (streams[i].connection.connectionId == session.connection.connectionId) {
+				return;
+			}
+			 
+			// Create the div to put the subscriber element in to
+			var div = document.createElement('div');
+			div.setAttribute('id', 'stream' + streams[i].streamId);
+			document.body.appendChild(div);
+			                   
+			// Subscribe to the stream
+			var subscribeProps = {height:240, width:320};
+			session.subscribe(streams[i], div.id);
+		}
+	}
+}
