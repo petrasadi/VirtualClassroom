514babdf055529b6339e4a6f7821e272f6d9251b
Andy Soderstrom <asoderst@gmail.com>
2012-10-20 16:26:41 -0500

    updated dao

---
+++ se491project/src/com/google/api/se491proj/josql/CategoryException.java
@@ -0,0 +1,24 @@
+package com.google.api.se491proj.josql;
+
+/**
+ * {@literal}
+ * class CategoryException
+ * 
+ * @author
+ * Adrian Petras <petrasadi@gmail.com>
+ * Andy Soderstrom <asoderst@gmail.com>
+ * Casey Benzel <casey.benzel@gmail.com>
+ * Elizabeth Stovall <emstovall@gmail.com>
+ * James Raitsev <raitsev@gmail.com>
+ *
+ */
+@SuppressWarnings("serial")
+public class CategoryException extends Exception {
+	public CategoryException(String errorMessage) {
+		super(errorMessage);
+	}
+
+	public CategoryException(Throwable throwable) {
+	    super(throwable);
+	}
+}
