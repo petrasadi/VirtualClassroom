b318c5831afb12440634ba149a032e9fc01d5a97
Andy Soderstrom <asoderst@gmail.com>
2012-11-07 21:27:58 -0600

    adding another command and updating classregistration

---
+++ se491project/src/edu/depaul/se491/josqlCmds/IsClassFullCmd.java
@@ -0,0 +1,42 @@
+package edu.depaul.se491.josqlCmds;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+
+import edu.depaul.se491.josql.ClassesDAO;
+import edu.depaul.se491.josql.ClassesException;
+import edu.depaul.se491.josql.IClassesDAO;
+import edu.depaul.se491.model.Person;
+
+public class IsClassFullCmd implements IDaoCommands {
+	Key classId;
+	public IsClassFullCmd(Key classes) {
+		this.classId = classes;
+	}
+	public void execute() {
+		return;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public boolean isExecute() {
+		IClassesDAO classes = new ClassesDAO();
+		try {
+			Entity c = classes.getClassById(classId);
+			List<Key> slist = (List<Key>) c.getProperty("students");
+			if(((Integer) c.getProperty("maxstudents")).intValue() > slist.size()) {
+				return false;
+			} else {
+				return true;
+			}
+		} catch (ClassesException e) {
+			e.printStackTrace();
+			return true;
+		}
+	}
+	public LinkedList<Person> getExecute() {
+		return null;
+	}
+}
