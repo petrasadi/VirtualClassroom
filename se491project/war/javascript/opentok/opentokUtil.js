901f18eaa56656517dabf5796c592dc31ba803c6
Adrian Petras <petrasadi@gmail.com>
2012-11-10 01:08:16 -0600

    cleaned up handleOpenTok function

--- se491project/war/javascript/opentok/opentokUtil.js
+++ se491project/war/javascript/opentok/opentokUtil.js
@@ -1,8 +1,9 @@
 var session;
 var speakRequestsSet = { };
+var data;
 
-function handleOpenTok(data){
-	
+function handleOpenTok(dt){
+	data = dt;
 	var publisher;
 	
 	var apiKey = data.apiKey;
@@ -18,74 +19,6 @@ function handleOpenTok(data){
 	session.addEventListener('streamCreated', streamCreatedHandler);
 	session.addEventListener("signalReceived", raiseHandHandler);
 	session.connect(apiKey, token);
-
-	function sessionConnectedHandler(event) {
-		//only a teacher can publish when connecting to the session
-		if(userRole == "teacher"){
-			var div = document.createElement('div');
-			div.setAttribute('id', 'publisher');
-			$('#myPublisherDiv').append(div);
-			publisher = TB.initPublisher(apiKey, div);
-	
-			//TODO - only add publisher if camera and mic detected
-	
-			publisher.addEventListener('accessAllowed', accessAllowedHandler);
-			session.publish(publisher);
-		}
-
-		// Subscribe to streams that were in the session when we connected
-		subscribeToStreams(event.streams);
-	}
-
-	function streamCreatedHandler(event) {
-		// Subscribe to any new streams that are created
-		subscribeToStreams(event.streams);
-	}
-
-	function subscribeToStreams(streams) {
-		//display a generic message if professor is not connected yet
-		if (streams.length == 0 &&
-				userRole != "teacher"){
-			$('#subscribers').append("<h4 id=\"noSession\">Waiting for instructor to start the session.</h4>");
-		} else {
-			$('#noSession').remove();
-			for (var i = 0; i < streams.length; i++) {
-				// Create the div to put the subscriber element in to
-				var div = document.createElement('div');
-				div.setAttribute('id', 'stream' + streams[i].streamId);
-				$('#subscribers').append(div);
-
-				// Subscribe to the stream
-				var subscribeProps = {width:400, height:225};
-				session.subscribe(streams[i], div.id, subscribeProps);
-			}
-			generateUserDashBoard(data.role);
-		}
-	}
-	
-	//TODO - fix raiseHand event
-	function raiseHandHandler(event){
-		var connectionId = event.fromConnection.connectionId;
-		//ignore request if already registered
-		if (!(connectionId in speakRequestsSet) ||
-				(speakRequestsSet[connectionId] === false)){
-			speakRequestsSet[connectionId] = true;
-
-			var rqstParams = "'" + data.role + "', '" + connectionId + "'";
-			var btnId = "requestButton" + connectionId;
-			var element = '<input class="dashboardButton" ';
-			element += 'id="' + btnId + '"';
-			element += 'type="image" src="images/opentok/face.png" title="student speak request"';
- 			element += 'onclick="switchPublishingUser(' + rqstParams + ')"';
-			element += '>';
-			$('#speakRequests').append(element);
-		}
-	}
-
-	function accessAllowedHandler(event){
-		//make the publisher img invisible after receiving access
-		$("#myPublisherDiv").addClass("invisible");
-	}
 }
 
 
@@ -101,4 +34,79 @@ function switchPublishingUser(userRole, connectionId){
 
 function raisehand(){
 	session.signal();
+/*	var stateManager = session.getStateManager();
+	stateManager.addEventListener("changed:raiseHand", testRaiseHandHandler);
+	stateManager.set("raiseHand", "bar");*/
+}
+
+//TODO - fix raiseHand event
+function raiseHandHandler(event){
+	var connectionId = event.fromConnection.connectionId;
+	//ignore request if already registered
+	if (!(connectionId in speakRequestsSet) ||
+			(speakRequestsSet[connectionId] === false)){
+		speakRequestsSet[connectionId] = true;
+
+		var rqstParams = "'" + data.role + "', '" + connectionId + "'";
+		var btnId = "requestButton" + connectionId;
+		var element = '<input class="dashboardButton" ';
+		element += 'id="' + btnId + '"';
+		element += 'type="image" src="images/opentok/face.png" title="student speak request"';
+			element += 'onclick="switchPublishingUser(' + rqstParams + ')"';
+		element += '>';
+		$('#speakRequests').append(element);
+	}
+}
+
+function testRaiseHandHandler(event){
+	alert("success");
+}
+
+function subscribeToStreams(streams) {
+	//display a generic message if professor is not connected yet
+	if (streams.length == 0 &&
+			data.role != "teacher"){
+		$('#subscribers').append("<h4 id=\"noSession\">Waiting for instructor to start the session.</h4>");
+	} else {
+		$('#noSession').remove();
+		for (var i = 0; i < streams.length; i++) {
+			// Create the div to put the subscriber element in to
+			var div = document.createElement('div');
+			div.setAttribute('id', 'stream' + streams[i].streamId);
+			$('#subscribers').append(div);
+
+			// Subscribe to the stream
+			var subscribeProps = {width:400, height:225};
+			session.subscribe(streams[i], div.id, subscribeProps);
+		}
+		generateUserDashBoard(data.role);
+	}
+}
+
+function accessAllowedHandler(event){
+	//make the publisher img invisible after receiving access
+	$("#myPublisherDiv").addClass("invisible");
+}
+
+function sessionConnectedHandler(event) {
+	//only a teacher can publish when connecting to the session
+	if(data.role == "teacher"){
+		var div = document.createElement('div');
+		div.setAttribute('id', 'publisher');
+		$('#myPublisherDiv').append(div);
+		publisher = TB.initPublisher(data.apiKey, div);
+
+		//TODO - only add publisher if camera and mic detected
+
+		publisher.addEventListener('accessAllowed', accessAllowedHandler);
+		session.publish(publisher);
+	}
+
+	// Subscribe to streams that were in the session when we connected
+	subscribeToStreams(event.streams);
+}
+
+function streamCreatedHandler(event) {
+	// Subscribe to any new streams that are created
+	subscribeToStreams(event.streams);
 }
