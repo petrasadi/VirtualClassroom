a1e454c3da3ebfceb4981d6aedac1544a23a66b6
James Raitsev <raitsev@gmail.com>
2013-02-04 18:04:40 -0600

    Added a stub for a VirtualClassTest

---
+++ se491project/src/test/java/edu/depaul/se491/engine/VirtualClassTest.java
@@ -0,0 +1,64 @@
+package edu.depaul.se491.engine;
+
+import org.junit.Test;
+
+/**
+ * User: James Raitsev
+ * Date: 2/4/13
+ */
+public class VirtualClassTest
+{
+    @Test
+    public void testAddToWorkload() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetStudents() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testIsClassInSession() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testToString() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testAddStudent() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetInstructor() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testRemove() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testHashCode() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testEquals() throws Exception
+    {
+
+    }
+}
