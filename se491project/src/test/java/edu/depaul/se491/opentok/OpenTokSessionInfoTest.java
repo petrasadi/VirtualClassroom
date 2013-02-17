1c6d9110dafd7b48a00cc8625e59388c1fa5bb96
James Raitsev <raitsev@gmail.com>
2013-02-16 18:05:00 -0600

    Added OpenTokSessionInfoTests

--- se491project/src/test/java/edu/depaul/se491/opentok/OpenTokSessionInfoTest.java
+++ se491project/src/test/java/edu/depaul/se491/opentok/OpenTokSessionInfoTest.java
@@ -1,8 +1,27 @@
 package edu.depaul.se491.opentok;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * User: James Raitsev
  * Date: 2/11/13
  */
-public class OpenTokSessionInfoTest {
+public class OpenTokSessionInfoTest
+{
+
+    @Test
+    public void testGettersAndSetters() throws Exception
+    {
+        OpenTokSessionInfo o = new OpenTokSessionInfo();
+        o.setRole("role");
+        o.setSessionId("session id");
+        o.setToken("token");
+
+        assertTrue(o.getRole().equals("role"));
+        assertTrue(o.getSessionId().equals("session id"));
+        assertTrue(o.getToken().equals("token"));
+
+    }
 }
